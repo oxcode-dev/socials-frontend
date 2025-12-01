a06d460db740ce738f640ce25daa7f5ea36c906f
oxcode <mrexcelsam1@live.com>
2025-12-02 00:31:30 +0100

    initials

---
+++ app/auth/layout.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+// import { Logo } from '@/components/Logo';
+// import useRedirectIfAuthenticated from '@/hooks/useRedirectIfAuthenticated';
+import Image from 'next/image'
+import Link from 'next/link';
+import React, { Children } from 'react'
+
+const layout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+   
+    // useRedirectIfAuthenticated('/dashboard')
+
+    return (
+        <div>
+            <div className="relative">
+
+                <div className="relative w-full h-screen">
+                    <div className="w-full h-full bg-white min-h-screen bg-linear-to-br from-0% to-60% from-green-50 to-white">
+                        <div className="flex w-full md:max-w-lg mx-auto px-4 flex-col items-center justify-center h-full">
+                            <div className="py-6 px-4 md:px-8">
+                                <Link href="/">
+                                    {/* <Logo /> */}
+                                </Link>
+
+                                <div>
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default layout;
---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+const Login = () => {
+    return (
+        <>
+            <div className="text-black">
+                Hello Login
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus distinctio consequatur illo ratione architecto, accusantium aliquid. At autem quas, velit a cupiditate ducimus? Earum magni saepe quas debitis autem voluptatum!
+            </div>
+        </>
+    )
+}
+
+export default Login
