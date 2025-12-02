24f0a19d149d4da0713758e326eefa18c8214642
oxcode <mrexcelsam1@live.com>
2025-12-02 23:30:39 +0100

    initials

--- forms/auth/SignUpForm.tsx
+++ forms/auth/SignUpForm.tsx
@@ -19,27 +19,6 @@ const SignUpForm = () => {
                     <input type="password" placeholder="********" className="input bg-white border-gray-500" />
                 </div>
 
-                <div className="flex justify-between items-center py-1.5">
-                    <div>
-                        <label className="label text-sm">
-                            <input 
-                                type="checkbox" 
-                                defaultChecked
-                                className="checkbox checkbox-primary checked:text-white checked:bg-primary checked:border-transparent checkbox-sm" 
-                            />
-                            Remember me
-                        </label>
-                    </div>
-
-                    <div>
-                        <Link href="/auth/forgot-password" className="text-sm text-gray-500 font-medium underline">
-                            Forgot Password?
-                        </Link>
-                    </div>
-
-                </div>
-
-
                 <div className="w-full">
                     <button className="btn btn-primary w-full">Sign In</button>
                 </div>
