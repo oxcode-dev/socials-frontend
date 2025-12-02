14025d2f02a9830b5f8f5b07ea1bcf0cbf178c6e
oxcode <mrexcelsam1@live.com>
2025-12-02 23:51:18 +0100

    initials

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,13 @@
-import { ShoppingCartIcon } from '@heroicons/react/20/solid'
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/20/solid'
 import React from 'react'
 
 export const Logo = () => {
     return (
-        <div className="inline-flex space-x-2 items-center text-green-600">
+        <div className="inline-flex space-x-2 items-center text-gray-600">
             <div>
-                <ShoppingCartIcon className="size-8" />
+                <ChatBubbleLeftRightIcon className="size-8" />
             </div>
-            <div className="text-sm font-semibold">
+            <div className="text-lg font-semibold">
                 <p>SOCIALS</p>
             </div>
         </div>
