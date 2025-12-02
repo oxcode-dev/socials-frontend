b189de7a3c9627f5040f30f2b3bc1cecafc0b37a
oxcode <mrexcelsam1@live.com>
2025-12-03 00:06:41 +0100

    initials

--- forms/auth/SignUpForm.tsx
+++ forms/auth/SignUpForm.tsx
@@ -13,6 +13,18 @@ const SignUpForm = () => {
 
             <form className="flex flex-col w-full space-y-4 text-gray-500">
                 <div>
+                    <label htmlFor="first_name" className="font-medium text-gray-500 mb-1">First Name</label>
+                    <input type="text" id="first_name" placeholder="First Name" className="input bg-white border-gray-500" />
+                </div>
+                <div>
+                    <label htmlFor="last_name" className="font-medium text-gray-500 mb-1">Last Name</label>
+                    <input type="text" id="last_name" placeholder="Last Name" className="input bg-white border-gray-500" />
+                </div>
+                <div>
+                    <label htmlFor="username" className="font-medium text-gray-500 mb-1">Username</label>
+                    <input type="text" id="username" placeholder="Your Username" className="input bg-white border-gray-500" />
+                </div>
+                <div>
                     <label htmlFor="email" className="font-medium text-gray-500 mb-1">Email</label>
                     <input type="email" id="email" placeholder="Your Email" className="input bg-white border-gray-500" />
                 </div>
