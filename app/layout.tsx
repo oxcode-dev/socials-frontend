d247ee250a8be3f75b039a76af4ccabf32db4d1d
oxcode <mrexcelsam1@live.com>
2025-12-04 00:43:04 +0100

    initials

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ToastContextProvider>
+          {children}
+        </ToastContextProvider>
       </body>
     </html>
   );
