dbee1ab33e0593bee5bdb84d87c4abec40b06454
oxcode <mrexcelsam1@live.com>
2025-12-19 01:40:22 +0100

    initials

--- types/index.ts
+++ types/index.ts
@@ -11,10 +11,13 @@ export interface BreadcrumbItem {
 export interface User {
     id: number;
     first_name: string;
+    username: string;
     name: string;
     last_name: string;
     email: string;
     phone: string;
     avatar?: string;
+    is_private: boolean
+    verified: boolean
     [key: string]: unknown; // This allows for additional properties...
 }
