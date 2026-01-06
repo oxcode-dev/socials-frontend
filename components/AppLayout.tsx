28e0582c8b3d146a68a58d95bdce352cd9954bfc
Oxcode <mrexcelsam1@gmail.com>
2026-01-06 17:31:17 +0100

    initials

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,7 +1,11 @@
+import Sidebar from "./Sidebar";
+
 export default function AppLayout () {
     return (
         <>
-        
+            <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+                <Sidebar />
+            </div>
         </>
     )
 }
