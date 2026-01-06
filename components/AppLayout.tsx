9ea6814119e5e45242b52f2441b239f7c92ab0fb
Oxcode <mrexcelsam1@gmail.com>
2026-01-06 23:53:04 +0100

    initials

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout ({ children }: PropType) {
         <>
             <div className="flex min-h-screen bg-zinc-50 font-sans">
                 <Sidebar />
-                <div>
+                <div className="w-full md:ml-64 text-gray-500">
                     {children}
                 </div>
             </div>
