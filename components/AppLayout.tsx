6478b9b5a894e8b01b823b4b68999d6df380066c
Oxcode <mrexcelsam1@gmail.com>
2026-01-07 14:50:58 +0100

    initials

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from "react";
 import Sidebar from "./Sidebar";
 
 type PropType = {
@@ -5,10 +8,12 @@ type PropType = {
 }
 
 export default function AppLayout ({ children }: PropType) {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <>
             <div className="flex min-h-screen bg-zinc-50 font-sans">
                 <Sidebar />
+                <div className="fixed md:hidden bg-gray-500/50 w-full h-full"></div>
                 <main className="w-full md:ml-64 text-gray-500">
                     <div className="p-2 md:p-4">
                         {children}
