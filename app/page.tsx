4fd98c6f7a265dd0da22ab63f92302ddce82bb1b
Oxcode <mrexcelsam1@gmail.com>
2026-01-08 23:06:58 +0100

    initials

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,29 @@ export default function Home() {
     <div>    
       <AppLayout>
 
-        <div className="w-full flex">
+        <div className="w-full flex flex-wrap">
           <div className="w-full md:w-2/3">
             <div>
-              
+              <div className="card bg-gray-100 w-full shadow-sm">
+                <figure className="px-4 pt-4">
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Shoes"
+                    className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">Card Title</h2>
+                  <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                  <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="w-full md:w-1/3"></div>
+          <div className="w-full md:w-1/3">
+            hello world
+          </div>
         </div>
       </AppLayout>
     </div>
