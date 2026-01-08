ff98f77e297937c9e0d2aae5ef1000ad000f9d51
Oxcode <mrexcelsam1@gmail.com>
2026-01-09 00:02:58 +0100

    initials

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AppLayout from "@/components/AppLayout";
 import Sidebar from "@/components/Sidebar";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
           <div className="w-full md:w-2/3">
             <div>
               <div className="card bg-gray-100 w-full shadow-sm px-4 py-2 space-y-2">
-                <div className="w-full flex">
+                <div className="w-full flex justify-between">
                   <div className="inline-flex space-x-2 items-center">
                     <img 
                       src="https://demo.foxthemes.net/instello/assets/images/avatars/avatar-7.jpg"
@@ -23,7 +24,23 @@ export default function Home() {
                     </div>
 
                   </div>
-                  <div></div>
+                  <div>
+                    <div className="dropdown dropdown-end">
+                      <div tabIndex={0} role="button" className="btn btn-circle btn-ghost btn-xs text-gray-600">
+                        <EllipsisHorizontalIcon className="size-5" />
+                      </div>
+                      <div
+                        tabIndex={0}
+                        className="card card-sm dropdown-content bg-base-100 rounded-box z-1 w-48 shadow-sm"
+                      >
+                        <div tabIndex={0} className="card-body">
+                          <h2 className="card-title">You needed more info?</h2>
+                          <p>Here is a description!</p>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
                 </div>
 
                 <figure className="">
