6db7b120959755bd2f2d766afd11e7e02c12c745
Oxcode <mrexcelsam1@gmail.com>
2026-01-09 22:59:45 +0100

    initials

--- app/page.tsx
+++ app/page.tsx
@@ -49,12 +49,9 @@ export default function Home() {
                     alt="Shoes"
                     className="rounded-xl" />
                 </figure>
-                <div className="card-body items-center text-center">
+                <div className="card-body">
                   <h2 className="card-title">Card Title</h2>
                   <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                  <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                  </div>
                 </div>
               </div>
             </div>
