c06c38205f19c7019126bf6267e933fc20a8a7d3
Oxcode <mrexcelsam1@gmail.com>
2026-01-10 14:40:52 +0100

    initials

---
+++ components/TimeLineCard.tsx
@@ -0,0 +1,83 @@
+import { BookmarkIcon, ChatBubbleOvalLeftEllipsisIcon, EllipsisHorizontalIcon, HeartIcon } from "@heroicons/react/20/solid";
+
+export default function TimeLineCard () {
+    return (
+        <>
+            <div>
+                <div className="card bg-gray-100 w-full shadow-sm px-4 py-2 space-y-2">
+                    <div className="w-full flex justify-between">
+                        <div className="inline-flex space-x-2 items-center">
+                            <img 
+                                src="https://demo.foxthemes.net/instello/assets/images/avatars/avatar-7.jpg"
+                                className="size-8 rounded-full object-cover"
+                            />
+                            <div>
+                                <p className="text-sm font-semibold">Osemeke Samuel</p>
+                                <p className="text-xs font-light text-gray-500">2 hours age</p>
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-circle btn-ghost btn-xs text-gray-600">
+                                    <EllipsisHorizontalIcon className="size-5" />
+                                </div>
+                                <div
+                                    tabIndex={0}
+                                    className="card card-sm dropdown-content bg-base-100 rounded-box z-1 w-48 shadow-sm"
+                                >
+                                    <div tabIndex={0} className="card-body">
+                                        <h2 className="card-title">You needed more info?</h2>
+                                        <p>Here is a description!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <figure className="">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="rounded-xl"
+                        />
+                    </figure>
+
+                    <div className="card-body p-2 px-0">
+                        <p className="text-sm">
+                            A card component has a figure, a body part, and inside body there are title and actions parts
+                        </p>
+                        <div className="flex justify-between items-center">
+                            <div className="inline-flex items-center space-x-2">
+                                <p className="inline-flex items-center space-x-2">
+                                    <span className="rounded-full bg-gray-200 shadow-md inline-flex items-center justify-center p-1">
+                                        <HeartIcon className="size-5" />
+                                    </span>
+                                    <span className="font-semibold">
+                                        1,300
+                                    </span>
+                                </p>
+                                <p className="inline-flex items-center space-x-2">
+                                    <span className="rounded-full bg-gray-200 shadow-md inline-flex items-center justify-center  p-1">
+                                        <ChatBubbleOvalLeftEllipsisIcon className="size-5" />
+                                    </span>
+                                    <span className="font-semibold">
+                                        1,530
+                                    </span>
+                                </p>
+                            </div>
+
+                            <div className="inline-flex items-center space-x-2">
+                                <p className="inline-flex items-center space-x-2">
+                                    <span className="rounded-full bg-gray-200 shadow-md inline-flex items-center justify-center  p-1">
+                                        <BookmarkIcon className="size-5" />
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
