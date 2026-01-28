bbe597b547e68ff2e37506cde25c7892371d4576
Oxcode <mrexcelsam1@gmail.com>
2026-01-28 23:47:47 +0100

    new system

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import AppLayout from "@/components/AppLayout";
-import Sidebar from "@/components/Sidebar";
 import TimeLineCard from "@/components/TimeLineCard";
-import { BookmarkIcon, ChatBubbleOvalLeftEllipsisIcon, HeartIcon } from "@heroicons/react/20/solid";
-import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>    
       <AppLayout>
 
-        <div className="w-full flex flex-wrap">
+        <div className="w-full flex flex-wrap px-2 md:px-8">
           <div className="w-full md:w-2/3">
-            <div className="space-y-6 py-4">
+            <div className="space-y-6 py-4 pt-0 pr-4">
               <TimeLineCard />
               <TimeLineCard />
               <TimeLineCard />
@@ -22,10 +18,150 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full md:w-1/3">
-            hello world
+            <div className="px-2 pl-4">
+
+              <div className="bg-white rounded-xl shadow-sm p-5 px-6 border1 dark:bg-dark2">
+                <div className="flex justify-between text-black dark:text-white">
+                  <h3 className="font-bold text-base"> Peaple You might know </h3>
+                  <button type="button">
+                    link
+                  </button>
+                </div>
+                <div className="space-y-4 capitalize text-xs font-normal mt-5 mb-2 text-gray-500 dark:text-white/80">
+                  <div className="flex items-center gap-3">
+                    <a href="profile.html">
+                      <img
+                        src="assets/images/avatars/avatar-7.jpg"
+                        alt=""
+                        className="bg-gray-200 rounded-full w-10 h-10"
+                      />
+                    </a>
+                    <div className="flex-1">
+                      <a href="profile.html">
+                        <h4 className="font-semibold text-sm text-black dark:text-white">
+                          {" "}
+                          Johnson smith
+                        </h4>
+                      </a>
+                      <div className="mt-0.5"> Suggested For You </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="text-sm rounded-full py-1.5 px-4 font-semibold bg-secondery"
+                    >
+                      {" "}
+                      Follow{" "}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <a href="profile.html">
+                      <img
+                        src="assets/images/avatars/avatar-5.jpg"
+                        alt=""
+                        className="bg-gray-200 rounded-full w-10 h-10"
+                      />
+                    </a>
+                    <div className="flex-1">
+                      <a href="profile.html">
+                        <h4 className="font-semibold text-sm text-black dark:text-white">
+                          {" "}
+                          James Lewis
+                        </h4>
+                      </a>
+                      <div className="mt-0.5"> Followed by Johnson </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="text-sm rounded-full py-1.5 px-4 font-semibold bg-secondery"
+                    >
+                      {" "}
+                      Follow{" "}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <a href="profile.html">
+                      <img
+                        src="assets/images/avatars/avatar-2.jpg"
+                        alt=""
+                        className="bg-gray-200 rounded-full w-10 h-10"
+                      />
+                    </a>
+                    <div className="flex-1">
+                      <a href="profile.html">
+                        <h4 className="font-semibold text-sm text-black dark:text-white">
+                          {" "}
+                          John Michael
+                        </h4>
+                      </a>
+                      <div className="mt-0.5"> Followed by Monroe</div>
+                    </div>
+                    <button
+                      type="button"
+                      className="text-sm rounded-full py-1.5 px-4 font-semibold bg-secondery"
+                    >
+                      {" "}
+                      Follow{" "}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <a href="profile.html">
+                      <img
+                        src="assets/images/avatars/avatar-3.jpg"
+                        alt=""
+                        className="bg-gray-200 rounded-full w-10 h-10"
+                      />
+                    </a>
+                    <div className="flex-1">
+                      <a href="profile.html">
+                        <h4 className="font-semibold text-sm text-black dark:text-white">
+                          {" "}
+                          Monroe Parker
+                        </h4>
+                      </a>
+                      <div className="mt-0.5"> Suggested For You </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="text-sm rounded-full py-1.5 px-4 font-semibold bg-secondery"
+                    >
+                      {" "}
+                      Follow{" "}
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <a href="profile.html">
+                      <img
+                        src="assets/images/avatars/avatar-4.jpg"
+                        alt=""
+                        className="bg-gray-200 rounded-full w-10 h-10"
+                      />
+                    </a>
+                    <div className="flex-1">
+                      <a href="profile.html">
+                        <h4 className="font-semibold text-sm text-black dark:text-white">
+                          {" "}
+                          Martin Gray
+                        </h4>
+                      </a>
+                      <div className="mt-0.5"> Suggested For You </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="text-sm rounded-full py-1.5 px-4 font-semibold bg-secondery"
+                    >
+                      {" "}
+                      Follow{" "}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </AppLayout>
     </div>
   );
 }
+
+
